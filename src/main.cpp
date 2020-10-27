d44421a4580d4a5d470294d03601bd5558dae38f
arituerto <aleritu@gmail.com>
2020-10-27 20:33:13 +0100

    current state added to web

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,33 @@
 #define NETWORK_NAME "GregoriaNet"
 #define NETWORK_PSWD "HansTiberioNacioEnEl2018"
 
+// AP VARIABLES
 IPAddress local_IP(10, 0, 0, 1);
 IPAddress gateway_IP(10, 0, 0, 1);
 IPAddress subnet(255, 255, 0, 0);
 
+//SERVER READING VARIABLES
 volatile float server_ref_temp = 25.0;
 volatile float server_th_temp = 3.0;
 volatile float server_loop_time = 30.0;
 volatile bool server_fermenter_on = false;
+
+// SERVER INSTANCE
+AsyncWebServer server(80);
+
+// TEMP CONTROL INSTANCE
+struct temp_control_handle_t tc_handle = {
+    .ref_temp = 43.0,
+    .th_temp = 3.0,
+    .relay_a = RELAY_A,
+    .relay_b = RELAY_B,
+    .relay_c = RELAY_C,
+    .warming_on = false,
+    .cooling_on = false,
+    .mode_change = false};
+
+// TEMP SENSOR INSTANCE
+struct temp_sensor_handle_t ts_handle;
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
@@ -40,8 +59,9 @@ const char index_html[] PROGMEM = R"rawliteral(
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
     html {font-family: Helvetivca; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
+    h1 {font-size: 3.0rem;}
+    h2 {font-size: 2.0rem;}
+    p {font-size: 1.0rem;}
     body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
     .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
     .switch input {display: none}
@@ -52,7 +72,8 @@ const char index_html[] PROGMEM = R"rawliteral(
   </style>
 </head>
 <body>
-  <h2>IoT Fermenter Web Server</h2>
+  <h1>IoT Fermenter Web Server</h1>
+  %CURRENTSTATE%
   %FORMPLACEHOLDER%
 <script>function toggleCheckbox(element) {
   var xhr = new XMLHttpRequest();
@@ -82,28 +103,37 @@ String processor(const String &var)
   if (var == "FORMPLACEHOLDER")
   {
     String output = "";
-    output += "<form action=\"/get\"> Reference Temperature: <input type=\"number\" name=\"ref_temp\" step=\"0.01\" min=\"10.0\" max=\"65.0\" value=\"" + float2string(server_ref_temp) + "\"> <input type=\"submit\" value=\"Submit\"> </form>";
-    output += "<form action=\"/get\"> Temperature threshold: <input type=\"number\" name=\"th_temp\" step=\"0.01\" min=\"0.5\" max=\"15.0\" value=\"" + float2string(server_th_temp) + "\"> <input type=\"submit\" value=\"Submit\"> </form>";
-    output += "<form action=\"/get\"> Loop time: <input type=\"number\" name=\"loop_time\" step=\"0.01\" min=\"5.0\" max=\"300.0\" value=\"" + float2string(server_loop_time) + "\"> <input type=\"submit\" value=\"Submit\"> </form>";
+    output += "<form action=\"/get\"> Reference Temperature: <input type=\"number\" name=\"ref_temp\" step=\"0.01\" min=\"10.0\" max=\"65.0\" value=\"" + float2string(server_ref_temp) + "\" size=\"20\"> <input type=\"submit\" value=\"Submit\"> </form>";
+    output += "<form action=\"/get\"> Temperature threshold: <input type=\"number\" name=\"th_temp\" step=\"0.01\" min=\"0.5\" max=\"15.0\" value=\"" + float2string(server_th_temp) + "\" size=\"20\"> <input type=\"submit\" value=\"Submit\"> </form>";
+    output += "<form action=\"/get\"> Loop time: <input type=\"number\" name=\"loop_time\" step=\"0.01\" min=\"5.0\" max=\"300.0\" value=\"" + float2string(server_loop_time) + "\" size=\"20\"> <input type=\"submit\" value=\"Submit\"> </form>";
     output += "<h4>Fermenting!!</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"fermenter_on\" " + bool2string(server_fermenter_on) + "><span class=\"slider\"></span></label>";
     return output;
   }
+  else if (var == "CURRENTSTATE")
+  {
+    String output = "";
+    output += "<div style=\"border: 1px solid black\">";
+    output += "<h1>Current state:</h1>";
+    output += "<p><b>Temperature</b>: " + float2string(temp_get(&ts_handle)) + "</p>";
+    output += "<p><b>Ref. Temperature</b>: " + float2string(tc_handle.ref_temp) + "</p>";
+    if (tc_handle.warming_on)
+    {
+      output += "<p>Peltier <b>WARMING</b></p>";
+    }
+    else if (tc_handle.cooling_on)
+    {
+      output += "<p>Peltier <b>COOLING</b></p>";
+    }
+    else
+    {
+      output += "<p>Peltier <b>OFF</b>/p>";
+    }
+    output += "</div>";
+    return output;
+  }
+
   return "";
 };
-
-AsyncWebServer server(80);
-
-struct temp_control_handle_t tc_handle = {
-    .ref_temp = 43.0,
-    .th_temp = 3.0,
-    .relay_a = RELAY_A,
-    .relay_b = RELAY_B,
-    .relay_c = RELAY_C,
-    .warming_on = false,
-    .cooling_on = false,
-    .mode_change = false};
-
-struct temp_sensor_handle_t ts_handle;
 
 void start()
 {
@@ -147,7 +177,7 @@ void start()
       inputValue = NAN;
       inputParam = "none";
     }
-    // Serial.printf("/GET %s - %s : %5.2f\n", inputParam, inputMessage, inputValue);
+    Serial.printf("/GET %s - %s : %5.2f\n", inputParam, inputMessage, inputValue);
     request->redirect("/");
     // request->send(200, "text/html", "OK");
   });
@@ -162,6 +192,10 @@ void start()
       if (output == "fermenter_on")
       {
         server_fermenter_on = state.toInt();
+        if (server_fermenter_on == 0)
+        {
+          temp_control_set_off(&tc_handle);
+        }
       }
     }
     else
@@ -169,7 +203,7 @@ void start()
       output = "none";
       state = "none";
     }
-    // Serial.printf("/UPDATE %s: %s\n", output, state);
+    Serial.printf("/UPDATE %s: %s\n", output, state);
     request->redirect("/");
     // request->send(200, "text/plain", "OK");
   });
@@ -227,7 +261,10 @@ void loop()
   float th_temp = server_th_temp;
   set_temp_th(&tc_handle, th_temp);
 
-  temp_control_run(&ts_handle, &tc_handle);
+  if (server_fermenter_on)
+  {
+    temp_control_run(&ts_handle, &tc_handle);
+  }
 
   Serial.printf("server_fermenter_on = %s\n", server_fermenter_on ? "true" : "false");
 
